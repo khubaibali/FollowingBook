bcf3acef6f68675a2fb9124c8bed94b700f59927
khubaibali <76751259+khubaibali@users.noreply.github.com>
2021-09-27 19:53:44 +0500

    Chapter4

--- chapter4/Program.cs
+++ chapter4/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
 namespace chapter4
-{
+{   class TryCatchException
+    {
+        public static void dividebyzero(int x,int y)
+        {
+            try
+            {
+                int result = x / y;
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)   // can also write DivideByZeroException
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Code is still alive");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TryCatchException.dividebyzero(2,0);
         }
     }
 }
