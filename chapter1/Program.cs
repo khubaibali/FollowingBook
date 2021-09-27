3fd367d4fa2b3df69815b361d5c2d4df58e3f120
Syed Khubaib <khubaib.ali@qavitechnologies.com>
2021-09-27 19:26:23 +0500

    Initial commit

---
+++ chapter1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace chapter1
+{
+    class Program
+    {
+        static bool UseUmbrella(bool rainy, bool sunny, bool windy)
+        {
+            return !windy && (rainy || sunny);
+        }
+        static void Main(string[] args)
+        {
+            //string msg = "C sharp great explaination";
+            //string uppermsg = msg.ToUpper();
+            //Console.WriteLine(uppermsg);
+
+            //int x = 2021;
+            //msg = msg + x.ToString();
+            //Console.WriteLine(msg);
+
+            //int x = 2021;
+            //Console.WriteLine(x);
+
+            //long y = x;
+            //Console.WriteLine(y);
+
+            //short z = (short)x;
+            //Console.WriteLine(z);
+
+            //Console.WriteLine(Program.UseUmbrella(true, false, true));
+            //char[] chaArr = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            //Console.WriteLine(chaArr);
+
+            //string str = null;
+            //str ??= "some default value";
+            //Console.WriteLine(str);
+
+        }
+    }
+}
---
+++ chapter3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chapter3
+{
+    public class Stack<T>
+    {
+        int position;
+        T[] data = new T[100];
+        public void Push(T obj);
+        public T Pop(T obj);
+    }
+    class Program
+    {
+        static void writecubes()
+        {   //defining function inside function
+            Console.WriteLine(cube(3));
+            Console.WriteLine(cube(2));
+
+            int cube(int x) => x * x *x;
+        }
+        static void Main(string[] args)
+        {
+            //Program.writecubes();
+            string name = "this is Syed Khubaib Ali";
+            string[] arr= name.Split();
+            var c = arr[..2];
+            Console.WriteLine(c);
+        }
+    }
+}
